df62206cec1682326669a497d282cea8e052ea12
Seveltarm <kacper100@hotmial.com>
2020-06-15 10:03:11 +0200

    list component rework

--- src/app/badges-list/badges-list.component.ts
+++ src/app/badges-list/badges-list.component.ts
@@ -1,6 +1,22 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { Subject } from 'rxjs';
 import { MatTabChangeEvent, MatTabGroup } from '@angular/material/tabs';
+
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+const getBadgesQuery = gql`
+  query badges ($category: Int!, $searchedPhrase: String) {
+    badges(category: $category, searchedPhrase: $searchedPhrase) {
+      _id
+      title
+      description
+      tasks
+      comment
+      logo
+    }
+  }
+`;
 @Component({
   selector: 'app-badges-list',
   templateUrl: './badges-list.component.html',
@@ -10,19 +26,49 @@ export class BadgesListComponent implements OnInit, AfterViewInit {
 
   @ViewChild('tabGroup') tabGroup: MatTabGroup;
 
-  public indexValue: Subject<number> = new Subject();
+  public badges: Subject<number> = new Subject();
 
-  constructor() { }
+  constructor(
+    private apollo: Apollo
+  ) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit() {
-    this.indexValue.next(this.tabGroup.selectedIndex);
+    this.getBadges(this.tabGroup.selectedIndex)
   }
 
   public getTabIndex($event: MatTabChangeEvent): void {
-    this.indexValue.next($event.index);
+    this.getBadges($event.index);
+  }
+  
+  private getBadges(currentIndex: number): void {
+    this.apollo.query({
+      query: getBadgesQuery,
+      variables: {
+        category: currentIndex
+      }
+    }).subscribe(({ data }) => {
+      const response: any = data;
+      this.badges.next(response.badges);
+    }, (error) => {
+      console.log('error', error)
+    });
+  }
+
+  public getFilteredBadges($event: string): void {
+    this.apollo.query({
+      query: getBadgesQuery,
+      variables: {
+        category: this.tabGroup.selectedIndex,
+        searchedPhrase: $event
+      }
+    }).subscribe(({ data }) => {
+      const response: any = data;
+      this.badges.next(response.badges);
+    }, (error) => {
+      console.log('error', error)
+    });
   }
 
 }
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,22 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Subject, Subscription } from 'rxjs';
-
-import { Apollo } from 'apollo-angular';
-import gql from 'graphql-tag';
-
-const getBadgesQuery = gql`
-  query badges ($category: Int!, $searchedPhrase: String) {
-    badges(category: $category, searchedPhrase: $searchedPhrase) {
-      _id
-      title
-      description
-      tasks
-      comment
-      logo
-    }
-  }
-`;
 
 @Component({
   selector: 'app-list',
@@ -26,25 +10,17 @@ const getBadgesQuery = gql`
 export class ListComponent implements OnInit {
   public list: any;
   private subscription: Subscription;
-  private currentIndex: Number;
 
-  @Input() index: Subject<any>;
-  @Input() listType: string;
+  @Input() incomingData: Subject<any>;
+  @Output() searchedPhrase: EventEmitter<string> = new EventEmitter();
 
   constructor(
     private sanitizer: DomSanitizer,
-    private apollo: Apollo
-
   ) { }
 
   ngOnInit(): void {
-    this.subscription = this.index.subscribe(selectedIndex => {
-      this.currentIndex = selectedIndex;
-      switch(this.listType) {
-        case 'badges':
-          this.getBadges();
-          break;
-      }
+    this.subscription = this.incomingData.subscribe(listData => {
+      this.list = listData;
     });
   }
 
@@ -52,36 +28,11 @@ export class ListComponent implements OnInit {
     this.subscription.unsubscribe();
   }
 
-  public getBadges(): void {
-    this.apollo.query({
-      query: getBadgesQuery,
-      variables: {
-        category: this.currentIndex
-      }
-    }).subscribe(({ data }) => {
-      const response: any = data;
-      this.list = response.badges
-    }, (error) => {
-      console.log('error', error)
-    });
+  public filterList($event: string): void {
+    this.searchedPhrase.emit($event);
   }
 
-  public filterList($event: string) {
-    this.apollo.query({
-      query: getBadgesQuery,
-      variables: {
-        category: this.currentIndex,
-        searchedPhrase: $event
-      }
-    }).subscribe(({ data }) => {
-      const response: any = data;
-      this.list = response.badges
-    }, (error) => {
-      console.log('error', error)
-    });
-  }
-
-  public transformBaseToImage(base64Image: string){
+  public transformBaseToImage(base64Image: string) {
     if (base64Image) {
       return this.sanitizer.bypassSecurityTrustResourceUrl(base64Image);
     }  
